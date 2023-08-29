7601a714218493c26007512e772775d9d242a8eb
sultonoir <sultonalidrus5@gmail.com>
2023-08-30 02:20:03 +0700

    add webhoock

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,9 @@
 import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />;
+  return (
+    <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+      <SignIn />
+    </div>
+  );
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,7 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+    <SignUp />
+  </div>;
 }
